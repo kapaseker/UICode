4f96fe559663f70c124cb4dc5ac870ba6e60f8d8
kapaseker <panoogunker@gmail.com>
2017-06-12 18:49:01 +0800

    add map samples

--- pages/samples/scenicview/scenicview.js
+++ pages/samples/scenicview/scenicview.js
@@ -1,35 +1,63 @@
+var Calc = require("../../../util/calc.js");
 
-
+// touch mode include "move","pinch"
 Page({
     data: {
-        picwidth: 3024,
-        picheight: 2268,
+        picwidth: 0,
+        picheight: 0,
+        picLength: 0,
         topOffset: 0,
         leftOffset: 0,
-        lastX: 0,
-        lastY: 0,
+        evtMode: "move",
+        moveEvent: {
+            lastX: 0,
+            lastY: 0,
+        },
+        pinchEvent: {
+            distance: 0,
+            center: {
+                x: 0,
+                y: 0
+            }
+        },
+        xRatio: 1,
+        yRatio: 1,
     },
 
-    onRead: function () {
+    onReady: function () {
 
+        var width = 3024;
+        var height = 2268;
+
+        this.setData({
+            picwidth: width,
+            picheight: height,
+            picLength: Calc.distanceInTowPoints(0, 3024, 0, 2268),
+        });
     },
 
     onToucStart: function (evt) {
         if (evt.touches.length == 1) {
-            this.setLastTouch(evt);
+            this.setData({
+                evtMode: "move",
+            })
+            this.setLastMovePos(evt);
+        } else if (evt.touches.length == 2) {
+            this.setData({
+                evtMode: "pinch",
+            })
+            this.setLastPinchPos(evt.touches[0], evt.touches[1]);
         }
     },
 
     onMove: function (evt) {
-        if (evt.touches.length == 1) {
-
+        if (this.data.evtMode == "move") {
             var curX = evt.touches[0].clientX;
             var curY = evt.touches[0].clientY;
-            var lastX = this.data.lastX;
-            var lastY = this.data.lastY;
+            var lastPos = this.getLastMovePos();
+            var lastX = lastPos.x;
+            var lastY = lastPos.y;
 
-            console.log("last:[" + lastX + "," + lastY + "];cur:[" + curX + "," + curY + "]");
-            console.log("old leftOffSet:[" + this.data.leftOffset + "," + this.data.topOffset + "]");
             var setX = curX - lastX + this.data.leftOffset;
             var setY = curY - lastY + this.data.topOffset;
 
@@ -37,20 +65,100 @@ Page({
             setY = setY > 0 ? 0 : (setY < -this.data.picheight ? -this.data.picheight : setY);
 
             this.setPositionOffset(setX, setY);
+            this.setLastMovePos(evt);
+        } else if (this.data.evtMode == "pinch") {
 
-            this.setLastTouch(evt);
+            var pinPos = this.getLastPinchPos();
+            var lastDis = pinPos.distance;
+            var cent = pinPos.center;
+
+            var x0 = evt.touches[0].clientX;
+            var x1 = evt.touches[1].clientX;
+
+            var y0 = evt.touches[0].clientY;
+            var y1 = evt.touches[1].clientY;
+
+            var curDis = Calc.distanceInTowPoints(x0, x1, y0, y1);
+
+            var diffDis = Math.abs(curDis - lastDis);
+            var diffRatio = diffDis / this.data.picLength;
+
+            if (curDis < lastDis) { // shrink
+                this.setRatio(diffRatio, true);
+            } else if (curDis > lastDis) { // expand
+                this.setRatio(diffRatio, false);
+            }
+
+            this.setLastPinchPos(evt.touches[0], evt.touches[1]);
         }
     },
 
     onTouchEnd: function (evt) {
 
+        this.setData({
+            evtMode: "unset"
+        });
+
+        this.setLastPinchPos(null);
     },
 
-    setLastTouch: function (evt) {
+    setLastMovePos: function (evt) {
         this.setData({
-            lastX: evt.touches[0].clientX,
-            lastY: evt.touches[0].clientY
+            moveEvent: {
+                lastX: evt.touches[0].clientX,
+                lastY: evt.touches[0].clientY
+            }
         });
+    },
+
+    getLastMovePos: function () {
+        return {
+            x: this.data.moveEvent.lastX,
+            y: this.data.moveEvent.lastY
+        }
+    },
+
+    setLastPinchPos: function (lEvt, rEvt) {
+
+        if (lEvt == null || rEvt == null) {
+            // clear data
+            this.setData({
+                pinchEvent: {
+                    distance: 0,
+                    center: {
+                        x: 0,
+                        y: 0,
+                    }
+                }
+            });
+
+            return;
+        }
+
+        var x0 = lEvt.clientX;
+        var x1 = rEvt.clientX;
+
+        var y0 = lEvt.clientY;
+        var y1 = rEvt.clientY;
+
+        var dis = Calc.distanceInTowPoints(x0, x1, y0, y1);
+
+        var cent = this.data.pinchEvent.center;
+
+        if (this.data.pinchEvent.center == null) {
+            cent = Calc.centPointInTowPoints(x0, x1, y0, y1);
+        };
+
+        this.setData({
+            pinchEvent: {
+                distance: dis,
+                center: cent
+            }
+        })
+    },
+
+    getLastPinchPos: function () {
+        return this.data.pinchEvent;
     },
 
     setPositionOffset: function (x, y) {
@@ -58,6 +166,16 @@ Page({
             leftOffset: x,
             topOffset: y,
         });
-        console.log("cur pos:[" + x + "," + y + "]");
-    }
+    },
+
+    setRatio: function (ratio, isShrink) {
+        ratio = ratio * 10;
+
+        var realRation = isShrink ? (this.data.xRatio - ratio) : (this.data.xRatio + ratio);
+
+        this.setData({
+            xRatio: realRation,
+            yRatio: realRation,
+        });
+    },
 });
---
+++ util/calc.js
@@ -0,0 +1,14 @@
+var Calc = {
+    distanceInTowPoints(x0, x1, y0, y1) {
+        var dis = (Math.sqrt(((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0))));
+        return Math.round(dis * 100) / 100;
+    },
+    centPointInTowPoints(x0, x1, y0, y1) {
+        return {
+            x: ((x0 + x1) / 2),
+            y: ((y0 + y1) / 2)
+        };
+    }
+}
+
+module.exports = Calc;
